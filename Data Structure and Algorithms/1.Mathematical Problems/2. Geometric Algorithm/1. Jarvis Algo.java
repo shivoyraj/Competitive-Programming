9ccd5b74c35aed134d72c49fa3a1bec1449872c5
shivoyraj <shivamrajtripathi@hotmail.com>
2022-03-26 16:19:07 +0530

    jarvis algo

--- Data Structure and Algorithms/1.Mathematical Problems/2. Geometric Algorithm/1. Jarvis Algo.java
+++ Data Structure and Algorithms/1.Mathematical Problems/2. Geometric Algorithm/1. Jarvis Algo.java
@@ -7,12 +7,6 @@ class Point {
 		this.x = x;
 		this.y = y;
 	}
-
-	@Override
-	public String toString() {
-		return "Point [x=" + x + ", y=" + y + "]";
-	}
-	
 }
 
 class Solution {
@@ -44,12 +38,9 @@ class Solution {
 				if (orientation(points[p], points[i], points[q]) == 2)
 					q = i;
 			}
-
 			p = q;
-
-		} while (p != I); // While we don't come to first
-							// point
-
+		} while (p != I); // While we don't come to first point
+		
 		return hull;		
 	}
 }
@@ -57,16 +48,7 @@ class Solution {
 public class Test {
 	public static void main(String[] args)
 	{
-
-		Point points[] = new Point[7];
-		points[0]=new Point(2,2);
-		points[1]=new Point(4,3);
-		points[2]=new Point(5,4);
-		points[3]=new Point(0,3);
-		points[4]=new Point(2,1);
-		points[5]=new Point(0,0);
-		points[6]=new Point(4,0);
-		
+		Point points[] = {new Point(2,2),new Point(4,3),new Point(5,4),new Point(0,3),new Point(2,1),new Point(0,0),new Point(4,0)};
 		int n = points.length;
 		 Vector<Point> hull = Solution.convexHull(points, n);
 		for (Point temp : hull)
