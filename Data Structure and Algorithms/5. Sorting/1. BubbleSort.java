3b4ca8fd5443f4bb848f7f5912e82f1f111e1104
shivoyraj <87270143+shivoyraj@users.noreply.github.com>
2022-05-14 21:30:42 +0530

    Update 1. BubbleSort.java

--- Data Structure and Algorithms/5. Sorting/1. BubbleSort.java
+++ Data Structure and Algorithms/5. Sorting/1. BubbleSort.java
@@ -1,20 +1,28 @@
 class Solution {
 
-	public void bubbleSort(int[] a, int n) {
-		boolean isSwapped = false;
-		for (int i = 0; i < n; i++) {
-			for (int j = i; j < n; j++) {
-				if (a[j] < a[i]) {
-					int temp = a[j];
-					a[j] = a[i];
-					a[i] = temp;
-					isSwapped = true;
-				}
-			}
-			if (!isSwapped)
-				break;
-		}
-	}
+    public static void bubbleSort(int[] a){
+		
+        int i, j, temp;
+        int n = a.length-1;
+        boolean isSwapped=false;
+        
+        for (i = 0; i < n ; i++)
+        {
+            for (j = 0; j < n-i; j++)
+            {
+                if (a[j] > a[j + 1])
+                {
+                    temp = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = temp;
+                    isSwapped = true;
+                }
+            }
+ 
+            if (isSwapped == false)
+                break;
+        }
+    }
 
 }
 
@@ -22,7 +30,7 @@ class Solution {
 public class Test {
 	public static void main(String[] args) {
 		int[] a = { 8, 4, 2, 45, -3, 5, -54, 9, 5, 0 };
-		new Solution().bubbleSort(a,a.length);
+		new Solution().bubbleSort(a);
 		for (int i : a)
 			System.out.print(i + " ");
 	}
