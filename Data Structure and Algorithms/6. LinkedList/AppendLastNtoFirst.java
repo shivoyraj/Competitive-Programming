a2e2cf816594034964287d70591833f60b63983a
shivoyraj <shivamrajtripathi@hotmail.com>
2022-06-11 14:46:24 +0530

    lts

--- Data Structure and Algorithms/6. LinkedList/AppendLastNtoFirst.java
+++ Data Structure and Algorithms/6. LinkedList/AppendLastNtoFirst.java
@@ -11,31 +11,41 @@ class LinkedListNode<T> {
 
 class Solution {
 	
-	public static LinkedListNode<Integer> deleteNodeRec(LinkedListNode<Integer> head, int pos) {
+	public static LinkedListNode<Integer> appendLastNToFirst(LinkedListNode<Integer> head, int n) {
 		
-		if (head == null)
+        if (n == 0 || head == null) 
 			return head;
 		
-		if (pos == 0) 
-			return head.next;
+        LinkedListNode<Integer> fast = head;
+		LinkedListNode<Integer> slow = head;
+        
+		for (int i = 0; i < n && fast!=null; i++)
+			fast = fast.next;
+        
+		while (fast!=null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+        
+		LinkedListNode<Integer> newHead = slow.next;
+		slow.next = null;
+		fast.next = head;
 		
-		LinkedListNode<Integer> smallHead = deleteNodeRec(head.next, pos - 1);
-		head.next = smallHead;
-		return head;
+		return newHead;
+        
 	}
 }
 
 class Test {
 
 	public static void main(String[] args) {
-		
-		int[] a = {1,2,3,4,5,6,7,8,9,0};
-		
+
+		int[] a = {1,2,3,4,5,6,7,8,9};
+
 		LinkedListNode<Integer> head = AsList(a);
 		print(head);
-		head = Solution.deleteNodeRec(head,5);
+		head = Solution.appendLastNToFirst(head,4);
 		print(head);
-
 	}
 
 	public static void print(LinkedListNode<Integer> head) {
@@ -44,6 +54,7 @@ class Test {
 			System.out.print(head.data + " ->");
 			head = head.next;
 		}
+		
 		System.out.print("null");
 		System.out.println();
 
