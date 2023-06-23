7689a8df0b1dad23418107b221a9949efdbe35b3
shivoyraj <shivamrajtripathi@hotmail.com>
2023-06-23 14:48:06 +0530

    added minDepthofBST

---
+++ Data Structure and Algorithms/12. BST/16. MinDepthofBST.java
@@ -0,0 +1,36 @@
+// The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    public int minDepth(TreeNode root) {
+        
+        if(root ==null)
+            return 0;
+
+        if(root.left==null && root.right ==null)
+            return 1;
+        
+        if(root.left == null)
+            return 1 + minDepth(root.right);
+        
+        if(root.right == null)
+            return 1 + minDepth(root.left);
+
+        return 1+Math.min(minDepth(root.left),minDepth(root.right));
+    }
+}
